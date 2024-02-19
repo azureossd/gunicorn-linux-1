6ed81e101502def53c17b44c7bbfa75c8fc4e35c
Anthony Salemo <ajssalemo@gmail.com>
2024-02-19 16:06:20 -0500

    updating to the last version of fastapi and uvicorn, removing other deps and formatting

--- app.py
+++ app.py
@@ -8,5 +8,22 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
-    cities = [ { 'name': 'Dallas', 'temperature':'50F'}, { 'name': 'New York','temperature': '39F'} ,{'name': 'Mexico name','temperature': '69F'} , {'name': 'Vancouver','temperature':'45F'} , {'name': 'Sao Paulo','temperature':'76F'} , {'name': 'Buenos Aires','temperature':'85F'} , {'name': 'Frankfurt','temperature': '45F'} , {'name': 'Paris','temperature': '53F'} , {'name': 'Madrid','temperature':'42F'} , {'name': 'Berlin','temperature':'45F'} , {'name': 'Moscow','temperature': '15F'} ,{'name': 'Delhi','temperature':'54F'} ,{'name': 'Beijing','temperature':'22F'} , {'name': 'Tokyo','temperature': '33F'} , {'name': 'Sydney','temperature': '60F'} ]
+    cities = [
+        { 'name': 'Dallas', 'temperature':'50F'}, 
+        { 'name': 'New York','temperature': '39F'},
+        {'name': 'Mexico name','temperature': '69F'}, 
+        {'name': 'Vancouver','temperature':'45F'}, 
+        {'name': 'Sao Paulo','temperature':'76F'}, 
+        {'name': 'Buenos Aires','temperature':'85F'}, 
+        {'name': 'Frankfurt','temperature': '45F'}, 
+        {'name': 'Paris','temperature': '53F'}, 
+        {'name': 'Madrid','temperature':'42F'}, 
+        {'name': 'Berlin','temperature':'45F'}, 
+        {'name': 'Moscow','temperature': '15F'},
+        {'name': 'Delhi','temperature':'54F'},
+        {'name': 'Beijing','temperature':'22F'}, 
+        {'name': 'Tokyo','temperature': '33F'}, 
+        {'name': 'Sydney','temperature': '60F'} 
+    ]
+    
     return templates.TemplateResponse("item.html", {"request": request, "cities": cities})
